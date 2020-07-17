0af81b3a3d3c31ec8fe59c3c42a03fe06054c810
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-07-17 07:38:27 -0500

    Add files via upload

---
+++ Array_sort/Array_sort_down/Function/array_sort_down.js
@@ -0,0 +1,16 @@
+function ordonne_decroissant(arr){
+ for(let i = 0; i < arr.length; i++){
+  var a = arr[i];
+   for(let j = 0; j < arr.length; j++){
+    if( a > arr[j]){
+      alert("change to do");
+      var c = arr[i];
+      arr[i] = arr[j];
+      arr[j] = c; 
+    }
+   }
+ }
+return arr;
+}   
+var tab = [5,4,2,1,3]; 
+ordonne_decroissant(tab);
